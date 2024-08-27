3d6efed9f081d2c08e9fd15a8c2bab3fcd9724bd
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-27 11:42:57 +0700

    menulis dalam php html

---
+++ 3 menulis php dalam html .php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Selamat datang</h1>";
+echo "di smkn1 karang baru";
+echo "</body>";
+echo "</html>";
+echo "<br><br><strong>By fachri</strong>"
