2593f7fee5954eb0cb57250d73d6c940fa2b534a
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-28 10:14:34 +0700

    konversitipe data di php

---
+++ konversitipe data di php .php
@@ -0,0 +1,10 @@
+<?php
+$var = "123.45";
+$intvar = "123";
+$invtar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);// mengubah menjadi float
+echo $intvar;//output:123
+echo "<p>$floatvar";//output:123.45
+
+echo "<br><br><br><strong>By Fachri </strong>";
+?>
