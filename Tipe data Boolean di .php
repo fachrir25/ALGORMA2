90be8a929a4706e15fd129412e730106d55e3535
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-28 10:02:55 +0700

    tipe data array

---
+++ Tipe data Boolean di .php
@@ -0,0 +1,12 @@
+<?php
+//mendeklarasi variabel boolean
+$isActive = false;
+$menikah = true;
+//menggunakan boolean dalam pernyataan if
+if ($isActive){
+    echo "Nilai ini benar.";
+}else{
+    echo "Nilai ini salah.";
+}
+
+
