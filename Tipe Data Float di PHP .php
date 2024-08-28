e49665fa20f18c92501eb814ecf3ead75060628f
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-28 09:40:37 +0700

    tipe data float di php

---
+++ Tipe Data Float di PHP .php
@@ -0,0 +1,9 @@
+<?php
+$waktu = 233.12;
+$kecepatan = 13232.12;
+$jarak = 230.12 *13235.12 ;
+
+//batasi angka di belakang koma
+echo "<p>nilai jarak: $jarak";
+
+echo "<br><br><br><strong>By fachri </strong>";
